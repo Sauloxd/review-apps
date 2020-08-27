24eec7a18d9dd73e0cce45fe6dcdc78cd5306e30
sauloxd <saulotoshi@gmail.com>
2020-08-27 20:00:11 -0300

    Ready for usage

--- dist/index.js
+++ dist/index.js
@@ -1630,6 +1630,89 @@ exports.paginateRest = paginateRest;
 
 /***/ }),
 
+/***/ 343:
+/***/ (function(module, __unusedexports, __webpack_require__) {
+
+/* eslint-disable indent */
+const fs = __webpack_require__(747);
+
+const IndexPage = (reviewApps = {}, styles = '') => `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Review apps</title>
+    <meta name="description" content="QA every PR effortlessly">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style>
+      body { font-family: Roboto; }
+      ${styles}
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <h1 style="margin-top: 30px; margin-bottom: 15px;">
+            Static review apps collection
+          </h1>
+        </div>
+      </div>
+      ${Object.keys(reviewApps).length === 0 ? 'No apps available yet' : ''}
+      ${
+        Object.entries(reviewApps).map(([name, reviewApp]) => {
+          return `
+            <div class="row">
+              <div class="col-sm">
+                <h2 style="margin-top: 30px;">
+                  ${name}
+                </h2>
+              </div>
+            </div>
+            <div class="row">
+              ${Object.keys(reviewApp.apps).length === 0 ? 'No apps available yet' : ''}
+              ${reviewApp.apps.map((app, index) => {
+                return `
+                  <div class="col-sm">
+                    ${Card(app)}
+                  </div>
+                  ${index % 4 === 3 ?
+                   `
+                     <div class="w-100"></div>
+                   ` : ''}
+                `;
+
+              }).join('\n')}
+            </div>
+          `;
+        })
+.join('\n')
+      }
+    </div>
+  </body>
+</html>
+`;
+
+function Card(app) {
+  const { name, headCommitId, updatedAt, href, pullRequestUrl } = app;
+  return `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">#${headCommitId.substr(0, 12)}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">${updatedAt.toLocaleString()}</h6>
+        <a href="${href}" class="card-link">App</a>
+        ${pullRequestUrl ? `<a href="${pullRequestUrl}" class="card-link">Pull Request</a>` : ''}
+      </div>
+    </div>
+  `;
+}
+
+module.exports = IndexPage;
+
+
+/***/ }),
+
 /***/ 357:
 /***/ (function(module) {
 
@@ -3965,87 +4048,6 @@ exports.FetchError = FetchError;
 
 /***/ }),
 
-/***/ 459:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-/* eslint-disable indent */
-const fs = __webpack_require__(747);
-
-const IndexPage = (reviewApps, styles = '') => `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Review apps</title>
-    <meta name="description" content="QA every PR effortlessly">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <style>
-      body { font-family: Roboto; }
-      ${styles}
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm">
-          <h1 style="margin-top: 30px; margin-bottom: 15px;">
-            Static review apps collection
-          </h1>
-        </div>
-      </div>
-      ${
-        Object.entries(reviewApps).map(([name, reviewApp]) => {
-          return `
-            <div class="row">
-              <div class="col-sm">
-                <h2 style="margin-top: 30px;">
-                  ${name}
-                </h2>
-              </div>
-            </div>
-            <div class="row">
-              ${reviewApp.apps.map((app, index) => {
-                return `
-                  <div class="col-sm">
-                    ${Card(app)}
-                  </div>
-                  ${index % 4 === 3 ?
-                   `
-                     <div class="w-100"></div>
-                   ` : ''}
-                `;
-
-              }).join('\n')}
-            </div>
-          `;
-        })
-.join('\n')
-      }
-    </div>
-  </body>
-</html>
-`;
-
-function Card(app) {
-  const { name, headCommitId, updatedAt, href, pullRequestUrl } = app;
-  return `
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">#${headCommitId.substr(0, 12)}</h6>
-        <h6 class="card-subtitle mb-2 text-muted">${updatedAt.toLocaleString()}</h6>
-        <a href="${href}" class="card-link">App</a>
-        ${pullRequestUrl ? `<a href="${pullRequestUrl}" class="card-link">Pull Request</a>` : ''}
-      </div>
-    </div>
-  `;
-}
-
-module.exports = IndexPage;
-
-
-/***/ }),
-
 /***/ 463:
 /***/ (function(__unusedmodule, exports, __webpack_require__) {
 
@@ -4567,7 +4569,7 @@ const io = __webpack_require__(1);
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 const { exec } = __webpack_require__(986);
-const indexPage = __webpack_require__(459);
+const indexPage = __webpack_require__(343);
 
 try {
   createReviewApps();
@@ -4687,7 +4689,7 @@ function getParamsFromPayload() {
       headCommitId = payload.pull_request.head.sha;
       branchName = payload.pull_request.head.ref.split('/').pop();
       repositoryName = payload.repository.name;
-      pullRequestUrl = payload.pull_request.url;
+      pullRequestUrl = payload.pull_request.html_url;
     }
     if (['push'].includes(payload.action)) {
       userName = payload.pusher.name;
