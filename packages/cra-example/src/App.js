7ea05f8fccaf7e9435e377fb37cc7707a99e3e15
sauloxd <saulotoshi@gmail.com>
2020-08-31 20:33:41 -0300

    revert to yarn 1

--- packages/cra-example/src/App.js
+++ packages/cra-example/src/App.js
@@ -1,24 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
+import multishine from './assets/fox-multishine.gif';
+import waveshine from './assets/fox-waveshine.gif';
+import combo1 from './assets/fox-combo-1.gif';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <div style={{ marginTop: 24, marginBottom: 24 }}>
+            <Navbar />
+          </div>
+          <Switch>
+            <Route path={['/wave-shine']}>
+              <img src={waveshine}/>
+            </Route>
+            <Route path="/multi-shine">
+              <img src={multishine}/>
+            </Route>
+            <Route path="/combos">
+              <img src={combo1}/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <HashRouter>
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <div style={{ marginTop: 24, marginBottom: 24 }}>
+            <Navbar />
+          </div>
+          <Switch>
+            <Route path={['/wave-shine']}>
+              <img src={waveshine}/>
+            </Route>
+            <Route path="/multi-shine">
+              <img src={multishine}/>
+            </Route>
+            <Route path="/combos">
+              <img src={combo1}/>
+            </Route>
+          </Switch>
+        </div>
+      </HashRouter>
+    </>
+  );
+}
+
+function Navbar () {
+  const activeStyle = {
+    fontWeight: 'bold',
+    color: 'green'
+  };
+  const style={ marginRight: 24 };
+  return (
+    <div>
+      <NavLink
+        to="/wave-shine"
+        activeStyle={activeStyle}
+        style={style}
+      >
+        Wave Shine
+      </NavLink>
+      <NavLink
+        to="/multi-shine"
+        activeStyle={activeStyle}
+        style={style}
+      >
+        Multi-shine
+      </NavLink>
+      <NavLink
+        to="/combos"
+        activeStyle={activeStyle}
+      >
+        Combos
+      </NavLink>
     </div>
   );
 }
