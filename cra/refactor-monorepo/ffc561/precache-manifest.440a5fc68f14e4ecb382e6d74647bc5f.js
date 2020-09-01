de518024a14b6eb1caa1c933de930d7380f0f8ad
Sauloxd <saulo@qulture.rocks>
2020-09-01 14:11:45 +0000

    [skip ci] ref to ffc561bd644ba868e39f41ac9e591fd4b4959a1d for - cra

---
+++ cra/refactor-monorepo/ffc561/precache-manifest.440a5fc68f14e4ecb382e6d74647bc5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92464822a212b50bc288eb456ed75a13",
+    "url": "cra/refactor-monorepo/ffc561/index.html"
+  },
+  {
+    "revision": "887820a9a7d93c0dbd7e",
+    "url": "cra/refactor-monorepo/ffc561/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "cra/refactor-monorepo/ffc561/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "cra/refactor-monorepo/ffc561/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887820a9a7d93c0dbd7e",
+    "url": "cra/refactor-monorepo/ffc561/static/js/main.cfc0487d.chunk.js"
+  },
+  {
+    "revision": "110f4cc7238bb98a86f6",
+    "url": "cra/refactor-monorepo/ffc561/static/js/runtime-main.89ca8f23.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "cra/refactor-monorepo/ffc561/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "cra/refactor-monorepo/ffc561/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "cra/refactor-monorepo/ffc561/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
