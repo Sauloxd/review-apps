75a1bdc1b8ae885bebb42e075daaa0f40f1643a0
Sauloxd <saulo@qulture.rocks>
2020-09-02 18:05:54 +0000

    [skip ci] ref to f46991d53eee7b2ce03d4e5fb205e63a3d920c8e for - dev-cra

---
+++ dev-cra/master/f46991/precache-manifest.a0374f563d3d7c9444993f38c7acb925.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b32005fb009e9d6abeb9acce9f3f29",
+    "url": "/review-apps/dev-cra/master/f46991/index.html"
+  },
+  {
+    "revision": "84cf05202e3491097e6a",
+    "url": "/review-apps/dev-cra/master/f46991/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/master/f46991/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/master/f46991/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cf05202e3491097e6a",
+    "url": "/review-apps/dev-cra/master/f46991/static/js/main.ef546134.chunk.js"
+  },
+  {
+    "revision": "db06ca8bc41589af6911",
+    "url": "/review-apps/dev-cra/master/f46991/static/js/runtime-main.00c0c28f.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/master/f46991/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/master/f46991/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/master/f46991/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
