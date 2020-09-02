2b67b897ef975990dbeb0d4170a6c37b2731538c
Sauloxd <saulo@qulture.rocks>
2020-09-02 18:19:05 +0000

    [skip ci] ref to cd8013fd8365e0693987f22c2eff59e4fdd046c0 for - dev-cra

---
+++ dev-cra/update-readme/cd8013/precache-manifest.d16309e8ee5170bc326160a3d5118601.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eac32f1ed8ea056430023e76eda19a6",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/index.html"
+  },
+  {
+    "revision": "bdd77c5cb0c05f94ef79",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd77c5cb0c05f94ef79",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/js/main.edef5f4f.chunk.js"
+  },
+  {
+    "revision": "9262c9cd4a0619db4e4d",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/js/runtime-main.cc8bf46e.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/update-readme/cd8013/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
