893a5a4b8c50e038a3d0d5675a8059d897cd3bf0
Sauloxd <saulotoshi@gmail.com>
2021-12-21 12:20:11 +0000

    [skip ci] ref to 90fc9436c6726fc4ccd64b59943e0dc4851c4fd6 for - dev-cra

---
+++ dev-cra/ft-add-how-to-dev-docs/90fc94/precache-manifest.00e10fa6f1b3362e79cc3b1782e97d88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24955f6459b3712c22690262cec1621b",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/index.html"
+  },
+  {
+    "revision": "b2dbe8d8b6956125c453",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dbe8d8b6956125c453",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/js/main.2618bcde.chunk.js"
+  },
+  {
+    "revision": "4ec4723c6bd44b701cb0",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/js/runtime-main.e9a85c9f.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/ft-add-how-to-dev-docs/90fc94/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
