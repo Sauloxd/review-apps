a1379c8b9b148d2cd131ea7b51e3279336fd87b9
ReviewApps admin <review-apps@saulo.dev>
2021-12-26 16:40:52 +0000

    [skip ci] dev-cra - Updating app dev-cra/issue-8/branch/with/nested/names/9839db

--- packages/cra-example/build/service-worker.js
+++ packages/cra-example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/precache-manifest.6cd8d499a5ed9b4532c0a8f90333716f.js"
+  "/review-apps/dev-cra/issue-8/branch/with/nested/names/9839db/precache-manifest.3e70dde847c6c7f2d366ab9d75a5fad6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/review-apps/dev-cra/issue-8/branch/with/nested/names/9839db/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
