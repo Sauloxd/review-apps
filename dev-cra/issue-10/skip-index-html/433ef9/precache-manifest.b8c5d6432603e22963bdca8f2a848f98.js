14fc6f82c96e628d0dce42ecd244d0f47838494c
ReviewApps admin <review-apps@saulo.dev>
2021-12-29 20:57:06 +0000

    [skip ci] dev-cra - Updating app dev-cra/issue-10/skip-index-html/433ef9

---
+++ dev-cra/issue-10/skip-index-html/433ef9/precache-manifest.b8c5d6432603e22963bdca8f2a848f98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebd45ed2cd62d1933bb0d995b79a163",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/index.html"
+  },
+  {
+    "revision": "93db327e86570eb3a5eb",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93db327e86570eb3a5eb",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/js/main.df20ac7c.chunk.js"
+  },
+  {
+    "revision": "408c70bcfa2efdf14cce",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/js/runtime-main.71ed6757.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/ft-refactor-action/3a53c0/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
