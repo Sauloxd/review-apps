f9f8218e47cff911736fce969450b612e0f299d7
ReviewApps admin <review-apps@saulo.dev>
2021-12-29 20:16:55 +0000

    [skip ci] dev-cra - Updating app dev-cra/ft-deployment-status/69cba1

---
+++ dev-cra/ft-deployment-status/69cba1/precache-manifest.6cd8d499a5ed9b4532c0a8f90333716f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1c847b204b2a28cb310acd3bfc09ca",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/index.html"
+  },
+  {
+    "revision": "9f119b1e1a6a8aae2605",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f119b1e1a6a8aae2605",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/js/main.07f68130.chunk.js"
+  },
+  {
+    "revision": "396fe545a3e05102d2bf",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/js/runtime-main.366aaedb.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/íssue-8-nested-branch0names/5a4d8f/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
---
+++ dev-cra/ft-deployment-status/69cba1/precache-manifest.f6466ff4edc3f3abf57299e715b72512.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611bc5e8d0b090b7832b800a52d483e0",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/index.html"
+  },
+  {
+    "revision": "2bea82357565f422fcbe",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bea82357565f422fcbe",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/js/main.6e3c86d8.chunk.js"
+  },
+  {
+    "revision": "4ae52527753410e326fb",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/js/runtime-main.efe26ad3.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/ft-refactor-action/260237/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
