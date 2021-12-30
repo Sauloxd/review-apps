2b4ca787acbcff2e426fab2a8f072e269fb5f375
sauloxd <saulotoshi@gmail.com>
2021-12-30 10:49:33 -0300

    Sanitize userInput to avoid errors

--- packages/action/dist/utils/user-input.js
+++ packages/action/dist/utils/user-input.js
@@ -5,7 +5,7 @@ const core_1 = require("@actions/core");
 function userInput() {
     return {
         dist: (0, core_1.getInput)('dist'),
-        slug: (0, core_1.getInput)('slug'),
+        slug: ((0, core_1.getInput)('slug') || 'FAILED_TO_GET_SLUG').replace(/ /g, '-'),
         branch: (0, core_1.getInput)('branch'),
         buildCmd: (0, core_1.getInput)('build-cmd'),
         githubToken: (0, core_1.getInput)('GITHUB_TOKEN'),
--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -4,12 +4,16 @@ import * as handlers from './event-handlers';
 import { GithubTriggerActions } from './interface';
 import { withError } from './utils/log-error';
 import * as git from './utils/git';
+import { userInput } from './utils/user-input';
 
 export const run = withError(async function run() {
   debug(JSON.stringify(github, null, 2));
   const event = github.context.eventName as GithubTriggerActions;
+  const input = userInput();
 
   info('-> Review Apps start!');
+  info('-> Your input: ');
+  info(`-> ${JSON.stringify(input, null, 2)}`);
 
   await git.configure({
     name: 'ReviewApps admin',
