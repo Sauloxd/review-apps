58db15965eb008b57aaf3f0f47206b5c9371bb00
ReviewApps admin <review-apps@saulo.dev>
2021-12-30 13:52:11 +0000

    [skip ci] My-platform - Updating app My-platform/ft-showcase-review-app/2b4ca7

---
+++ My-platform/ft-showcase-review-app/2b4ca7/build/precache-manifest.4c42cb3c087d1481ecb806b1edb2c834.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a7e7d99c60f40986c8ea098a20c6bf",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/index.html"
+  },
+  {
+    "revision": "36ed206a3058cdb3a983",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ed206a3058cdb3a983",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/js/main.952d7526.chunk.js"
+  },
+  {
+    "revision": "41fea4507cc90384b189",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/js/runtime-main.e2e89bea.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/dev-cra/issue-10/skip-index-html/433ef9/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
