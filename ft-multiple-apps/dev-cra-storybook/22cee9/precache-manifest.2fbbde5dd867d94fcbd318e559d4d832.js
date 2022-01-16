4e109bcb9be2bf117bb8e1de08906d0cc4233b2f
ReviewApps admin <review-apps@saulo.dev>
2022-01-16 15:14:12 +0000

    [skip ci] - Updating branch ft-multiple-apps

---
+++ ft-multiple-apps/dev-cra-storybook/22cee9/precache-manifest.2fbbde5dd867d94fcbd318e559d4d832.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218106c57caf91c1698e2de3c60b5dbb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.098580f6d523041ff342.bundle.js"
+  },
+  {
+    "url": "runtime~main.098580f6d523041ff342.bundle.js"
+  },
+  {
+    "revision": "36723341cb846ddcdaa0",
+    "url": "static/css/main.cc6f50a5.chunk.css"
+  },
+  {
+    "revision": "d43eb968e25e81ce0af629bad1069a19",
+    "url": "static/media/code-brackets.d43eb968.svg"
+  },
+  {
+    "revision": "8104e6f5a95cbe9c9615b47daf881fc7",
+    "url": "static/media/colors.8104e6f5.svg"
+  },
+  {
+    "revision": "3f9a1398f6372bd43806b239c70f4759",
+    "url": "static/media/comments.3f9a1398.svg"
+  },
+  {
+    "revision": "d3dc567bd3e26a031bfd7fab1e765508",
+    "url": "static/media/direction.d3dc567b.svg"
+  },
+  {
+    "revision": "e3812c4ff33e4e7ae9686a7fcfef393f",
+    "url": "static/media/flow.e3812c4f.svg"
+  },
+  {
+    "revision": "5ba97e9be21511d352f0056b1970ce21",
+    "url": "static/media/plugin.5ba97e9b.svg"
+  },
+  {
+    "revision": "3452b916238c37efec707e6979db3556",
+    "url": "static/media/repo.3452b916.svg"
+  },
+  {
+    "revision": "66f4b748553f89f290a3247a913072af",
+    "url": "static/media/stackalt.66f4b748.svg"
+  },
+  {
+    "url": "vendors~main.098580f6d523041ff342.bundle.js"
+  },
+  {
+    "url": "vendors~main.098580f6d523041ff342.bundle.js.LICENSE.txt"
+  }
+]);
