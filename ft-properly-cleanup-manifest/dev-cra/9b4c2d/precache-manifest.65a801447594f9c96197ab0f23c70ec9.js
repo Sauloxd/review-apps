408f953cdc357259c98cea8bb42012b398c1347c
ReviewApps admin <review-apps@saulo.dev>
2022-03-16 13:39:51 +0000

    [skip ci] - Updating branch ft-properly-cleanup-manifest

---
+++ ft-properly-cleanup-manifest/dev-cra/9b4c2d/precache-manifest.65a801447594f9c96197ab0f23c70ec9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e3c5d9cb6da5d33447771a75b6b68e",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/index.html"
+  },
+  {
+    "revision": "4eb6705f0ee426849182",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb6705f0ee426849182",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/js/main.1f5ef037.chunk.js"
+  },
+  {
+    "revision": "dddb4cbf757ab3d4be71",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/js/runtime-main.d3c821ca.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/ft-properly-cleanup-manifest/dev-cra/9b4c2d/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
