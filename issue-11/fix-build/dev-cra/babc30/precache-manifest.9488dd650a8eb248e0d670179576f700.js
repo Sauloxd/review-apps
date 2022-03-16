d4530a4ca90b56b966ce3ee7493b9a6148de2a88
ReviewApps admin <review-apps@saulo.dev>
2022-03-16 12:38:16 +0000

    [skip ci] - Updating branch issue-11/fix-build

---
+++ issue-11/fix-build/dev-cra/babc30/precache-manifest.9488dd650a8eb248e0d670179576f700.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72899a0fe289dbebf0a6975666eb6ed3",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/index.html"
+  },
+  {
+    "revision": "04ae4c8abe3827b92f71",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "738666c0dbfc42132be8",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/js/2.a2a6c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/js/2.a2a6c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ae4c8abe3827b92f71",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/js/main.4c32b84d.chunk.js"
+  },
+  {
+    "revision": "c303f34dc55bb2950a57",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/js/runtime-main.71c411e3.js"
+  },
+  {
+    "revision": "b8000ebc499db701d63c5b20fa4ec8b5",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/media/fox-combo-1.b8000ebc.gif"
+  },
+  {
+    "revision": "90b071d480e7522de0f689bb21dba31a",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/media/fox-multishine.90b071d4.gif"
+  },
+  {
+    "revision": "7ec8b809d805e7c81c31a2e0d37e625e",
+    "url": "/review-apps/issue-11/fix-build/dev-cra/babc30/static/media/fox-waveshine.7ec8b809.gif"
+  }
+]);
